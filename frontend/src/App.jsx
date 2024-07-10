46f53357543144be3138d58ee7517009a457836a
Mina Ezz <minaezzat98@gmail.com>
2024-07-10 23:22:13 +0300

    2- UI design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// pages
+import {
+  SignupPage,
+  LoginPage,
+  HomePage,
+  NotificationPage,
+  ProfilePage,
+} from "./pages";
+// components
+import Sidebar from "./components/shared/Sidebar";
+import RightPanel from "./components/shared/RightPanel";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main className="flex max-w-6xl mx-auto">
+      {
+        // components are from UI-Design steps
+        // DONE: we need to install react router dom and react icons...
+        // DONE: we need to copy and paste signup and login pages
+        // DONE: we need to copy and paste the svg in X.jsx file
+        // DONE: we need to copy and paste home page
+        // TODO: we need to copy and paste create post component
+        // DONE: we need to copy and paste sidebar component
+        // REVIEW: dummy data in the utils folder
+        // DONE: we need to copy and paste right panel component
+        // REVIEW: skeleton components
+        // DONE: we need to copy and paste posts component
+        // DONE: we need to copy and paste notifications page
+        // REVIEW: loading spinner component
+        // DONE: we need to copy and paste profile page
+        // DONE: we need to copy and paste edit profile modal
+        // REVIEW: daisy ui inputs, modals, skeletons, loading spinner
+        // DONE: make sure that inputs of images accept only images
+        // DONE:commit the ui is done 2- UI design completed
+        // REVIEW: 3:31
+        //
+        /*
+         */
+      }
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+        </Routes>
+        <RightPanel />
+      </Router>
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import SignupPage from "./auth/signup/SignupPage";
+import LoginPage from "./auth/login/LoginPage";
+import HomePage from "./home/HomePage";
+import NotificationPage from "./notification/NotificationPage";
+import ProfilePage from "./profile/ProfilePage";
+
+export { SignupPage, LoginPage, HomePage, NotificationPage, ProfilePage };
